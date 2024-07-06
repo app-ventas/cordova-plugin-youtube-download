ec5530931c88dc0a2f2f6dc3e2094607f79bf688
app-ventas <161535562+app-ventas@users.noreply.github.com>
2024-07-06 00:27:55 -0500

    Update YoutubeDownloadClient.java

--- src/android/com/cordova/youtube/YoutubeDownloadClient.java
+++ src/android/com/cordova/youtube/YoutubeDownloadClient.java
@@ -5,34 +5,41 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import android.os.Environment;
-import android.util.Log;
+//import android.os.Environment;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
 
 public class YoutubeDownloadClient extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("downloadVideo")) {
-            String videoUrl = args.getString(0);
-            this.downloadVideo(videoUrl, callbackContext);
+            String videoId = args.getString(0);
+            //this.downloadVideo(videoId, callbackContext);
+            callbackContext.success("Video downloaded successfully to " + videoId);
+
             return true;
         }
         return false;
     }
 
-    private void downloadVideo(String videoUrl, CallbackContext callbackContext) {
+    /*private void downloadVideo(String videoId, CallbackContext callbackContext) {
+        String  msg  = '1.descargando videoId ' + videoId; 
         try {
             URL url = new URL(videoUrl);
+
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
 
+             msg  += '....2.URL connect';
+             msg  += '....3.GET PATH';
+
             String filePath = Environment.getExternalStorageDirectory().getPath() + "/Download/video.mp4";
             File file = new File(filePath);
+            msg  += '....4.GET filePath:'+filePath;
             FileOutputStream fileOutput = new FileOutputStream(file);
             InputStream inputStream = connection.getInputStream();
 
@@ -47,5 +54,5 @@ public class YoutubeDownloadClient extends CordovaPlugin {
         } catch (Exception e) {
             callbackContext.error("Failed to download video: " + e.getMessage());
         }
-    }
+    }*/
 }
